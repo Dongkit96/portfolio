a052fb9f6ef966b77fdfe074ff25329b4aacb7bc
Dongkit96 <@example.com>
2026-02-14 08:15:02 +0900

    //

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
-﻿const nextConfig = {
+﻿/** @type {import('next').NextConfig} */
+const repo = "portfolio";
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
     output: "export",
+    trailingSlash: true,           // 핵심
+    basePath: isProd ? "/portfolio" : "",
+    assetPrefix: isProd ? "/portfolio/" : "",
 };
 
 export default nextConfig;

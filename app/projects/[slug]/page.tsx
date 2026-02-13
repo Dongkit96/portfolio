04fa589340f4b2a8f8cd4e7b7dec2434186200d4
Dongkit96 <@example.com>
2026-02-14 08:55:10 +0900

    //

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -151,7 +151,7 @@ export default async function ProjectDetailPage({
                                 {/* eslint-disable-next-line @next/next/no-img-element */}
                                 <img
                                     key={`/${m.url}`}
-                                    src={src}
+                                    src={`${BASE_PATH}${m.url}`}
                                     alt={m.label}
                                     style={{ width: "50%", height: "50%", objectFit: "cover",borderRadius:12, }}
                                 />
